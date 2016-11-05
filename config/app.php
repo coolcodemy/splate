ff1c63baa883525b344aea553517087485d85f29
Ahmad Fikrizaman <ahmadfikrizaman@gmail.com>
2016-11-06 00:19:16 +0800

    asad

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
      */
 
-    'name' => 'splate',
+    'name' => 'splate jaksdad',
 
     'description' => 'Your SaaS Boilerplate, for Malaysian, by Malaysian',
 
